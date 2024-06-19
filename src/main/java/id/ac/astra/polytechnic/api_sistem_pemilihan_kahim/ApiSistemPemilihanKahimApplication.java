5cac6a7d5b6bfcac4d406362fee989a2756ef895
alamsyhptr <dp8792149@gmail.com>
2024-06-19 08:20:57 +0700

    Initiate

---
+++ src/main/java/id/ac/astra/polytechnic/api_sistem_pemilihan_kahim/ApiSistemPemilihanKahimApplication.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.api_sistem_pemilihan_kahim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemPemilihanKahimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSistemPemilihanKahimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/astra/polytechnic/api_sistem_pemilihan_kahim/ApiSistemPemilihanKahimApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.api_sistem_pemilihan_kahim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemPemilihanKahimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
